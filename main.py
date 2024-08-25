4feda4e39af531e31e7921904e69dfdd4c28276e
alperen <ozyukselalperen@gmail.com>
2024-08-25 17:39:04 +0300

    1

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+
+
+class DataStructures(object):
+
+    def __init__(self):
+
+        self.dizi1 = [0,1,2,3,4]
+        self.donguyazdir()
+
+    def donguyazdir(self):
+
+        for i in range(len(self.dizi1)):
+
+            print(self.dizi1[i])
+
+
+
+
+
+
+DataStructures()
