b0b0511cc14c462928b9b75c96468134b94a9fba
Alperen Ozyuksel <ozyukselalperen@gmail.com>
2024-08-31 22:52:32 +0300

    2

--- rl.py
+++ rl.py
@@ -3,11 +3,36 @@ import gym
 # Ortamı oluştur ve render_mode parametresini belirt
 env = gym.make("Taxi-v3", render_mode="ansi")
 
-# Ortamı başlat
-env.reset()
+# Q Table
 
-# Ortamı render et
-print(env.render())
+# Hyperparameter
 
-# Ortamı kapat
-env.close()
+# Plotting metrix
+
+episode_number = 10000
+
+for i in range(1,episode_number):
+
+    #Initilaze environment
+
+
+    while (True):
+
+        #exploit and explore to find action
+
+        #action procces and take reward/ take observation
+
+        # Q learning function
+
+        # Q Table update
+
+        # Update state
+
+        # Find wrong dropouts
+        if done:
+            break
+
+
+
+        #
+
